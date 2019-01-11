5b61eaf27979220f569781c885cbdc32a5ef7c67
semvs <semvs@ayvansaray.edu.tr>
2019-01-11 10:58:00 +0300

    Add project files.

---
+++ BLL/BusinessLogicLayer.cs
@@ -0,0 +1,34 @@
+﻿using DAL;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BLL
+{
+    public class BusinessLogicLayer
+    {
+        DataAccessLayer dal;
+        public BusinessLogicLayer()
+        {
+            dal = new DataAccessLayer();
+        }
+        //@ad = p.Ad, @soyad = p.Soyad, @telefon = p.Telefon, @email = p.email, @maas = p.maas, @sehir = p.sehir, @departman = p.departman 
+        public int PersonelKaydet(string ad, string soyad, string telefon, string email,int maas, string sehir, string departman)
+        {
+            Personel p = new Personel();
+            if (!string.IsNullOrEmpty(ad) && !string.IsNullOrEmpty(soyad) && !string.IsNullOrEmpty(email) && !string.IsNullOrEmpty(departman))
+            {
+                p.Ad = ad;
+                p.Soyad = soyad;
+                p.Telefon = string.IsNullOrEmpty(telefon)?"":telefon;
+                p.email = email;
+                p.maas = maas;
+                p.sehir = sehir;
+                p.departman = departman;
+            }
+            return 1;
+        }
+    }
+}
---
+++ DAL/DataAccessLayer.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Dapper;
+using System.Data.SqlClient;
+using System.Configuration;
+
+namespace DAL
+{
+    public class DataAccessLayer
+    {
+        SqlConnection con;
+        SqlCommand cmd;
+        SqlDataAdapter dap;
+        SqlDataReader rdr;
+        public DataAccessLayer()
+        {
+            con = new SqlConnection(ConfigurationManager.ConnectionStrings["SqlServer"].ConnectionString);
+
+        }
+        public int PersonelKaydet(Personel p)
+        {
+            string sql = "Insert into Calisan (Ad,Soyad,telefon,email,maas,sehir,departman)Values(@ad,@soyad,@telefon,@email,@maas,@sehir,@departman)";
+            con.Execute(sql, new { @ad = p.Ad, @soyad = p.Soyad, @telefon = p.Telefon, @email = p.email, @maas = p.maas, @sehir = p.sehir, @departman = p.departman });
+            return 1;
+        }
+
+    }
+}
